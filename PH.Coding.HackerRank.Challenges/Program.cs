6b7dcd428d89c081cdf49da22189dd79775943ab
PH <contact@phgomes.net>
2018-12-30 19:31:40 -0200

    Initial configuration

---
+++ PH.Coding.HackerRank.Challenges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PH.Coding.HackerRank.Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
